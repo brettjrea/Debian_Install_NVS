bf08249fce22b016791e143f2dc4c59c610dd8da
Brett Rea <6890798+brettjrea@users.noreply.github.com>
2023-02-27 00:57:40 -0500

    Update install-nvs.sh

--- install-nvs.sh
+++ install-nvs.sh
@@ -6,11 +6,12 @@ sudo apt update -y && sudo apt upgrade -y
 # remove unused packages
 sudo apt autoremove -y
 
-# install curl
-sudo apt install curl -y
 
-# install NVS
-git clone https://raw.githubusercontent.com/jasongin/nvs/master/install.sh ~/.nvs
+sudo apt install git -y
+export NVS_HOME="$HOME/.nvs"
+git clone https://github.com/jasongin/nvs "$NVS_HOME"
+. "$NVS_HOME/nvs.sh" install
+
 echo 'export NVS_HOME="$HOME/.nvs"' >> ~/.bashrc
 echo '[ -s "$NVS_HOME/nvs.sh" ] && . "$NVS_HOME/nvs.sh"  # This loads NVS' >> ~/.bashrc
 export NVS_HOME="$HOME/.nvs"
